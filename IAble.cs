77c41a6e75f75e4e1314951482bad3f08588742f
project2003 <projectalgoritmika123@gmail.com>
2022-12-19 12:40:21 +0400

    add

---
+++ IAble.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IAbles
+    {
+        public abstract void Add(object value);
+    }
+    interface IAddRangeable
+    {
+
+        public abstract void AddRange(ICollection c);
+    }
+    interface IRemoveable 
+    {
+        public abstract void Remove(object? obj);
+
+    }
+    interface IRemoveAtable
+    {
+        public abstract void RemoveAt(int index);
+
+
+    }
+    interface ITrimable
+    {
+        public abstract void Trim();
+
+    }
+
+}
